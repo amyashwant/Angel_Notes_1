1a7420728c449ccb577c5b625ca4282a1e32e6fe
amyashwant <poonamgangwar083@gmail.com>
2023-02-03 16:39:50 +0530

    qwefdf

--- src/App.js
+++ src/App.js
@@ -7,17 +7,13 @@ import NoteState from "./context/notes/NoteState";
 import { Alert } from "./components/Alert";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
-import { useState } from "react";
+
 
 function App() {
-  const [alert, setAlert] = useState(null);
+  // const [alert, setAlert] = useState(null);
   const showAlert = (message, type) => {
-    setAlert({
-      msg: message,
-      type: type,
-    });
     setTimeout(() => {
-      setAlert(null);
+     
     }, 3000);
   };
   return (
